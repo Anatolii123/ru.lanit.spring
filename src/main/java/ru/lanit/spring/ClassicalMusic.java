187e4a2760f78ec47781217bb23086ff9a1d7a47
AKusakin <anatolii.kusakin1234@gmail.com>
2019-10-17 16:07:19 +0500

    NinethCommit

--- src/main/java/ru/lanit/spring/ClassicalMusic.java
+++ src/main/java/ru/lanit/spring/ClassicalMusic.java
@@ -1,6 +1,9 @@
 package ru.lanit.spring;
 
 public class ClassicalMusic implements Music{
+
+    private ClassicalMusic() {}
+
     @Override
     public String getSong() {
         return "Adirididirdjiday";
--- src/main/java/ru/lanit/spring/RockMusic.java
+++ src/main/java/ru/lanit/spring/RockMusic.java
@@ -1,6 +1,10 @@
 package ru.lanit.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component("musicBean")
 public class RockMusic implements Music {
+
     @Override
     public String getSong() {
         return "OOO MOYA OBORONAAAA";
