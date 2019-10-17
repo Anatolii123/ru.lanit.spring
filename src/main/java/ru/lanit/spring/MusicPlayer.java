6d9f591b20886c9561cdaea75cd5b5203df940fd
AKusakin <anatolii.kusakin1234@gmail.com>
2019-10-17 12:43:56 +0500

    Second3Commit

--- src/main/java/ru/lanit/spring/MusicPlayer.java
+++ src/main/java/ru/lanit/spring/MusicPlayer.java
@@ -1,4 +1,14 @@
 package ru.lanit.spring;
 
 public class MusicPlayer {
+    private Music music;
+
+    //Inversion of Control
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
+    }
 }
